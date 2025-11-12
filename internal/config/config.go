b92107444b23208a0d876df97043ed47393c7017
Guntis Smaukstelis <gsmaukstelis@evolution.com>
2025-11-12 19:20:37 +0200

    Refactored so config has all paths

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,33 +1,56 @@
 package config
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 )
 
-var DataDir = getDataDir()
-var VideoDir = filepath.Join(DataDir, "video")
+// Paths holds all application directory paths
+type Paths struct {
+	Data        string
+	Video       string
+	SourceVideo string
+	Logs        string
+	Tmp         string
+}
+
+// Global paths instance - initialized once and reused everywhere
+var AppPaths = initPaths()
+
+func initPaths() *Paths {
+	dataDir := getDataDir()
+
+	return &Paths{
+		Data:        dataDir,
+		Video:       filepath.Join(dataDir, "video"),
+		SourceVideo: filepath.Join(dataDir, "sourceVideo"),
+		Logs:        filepath.Join(dataDir, "logs"),
+		Tmp:         filepath.Join(dataDir, "tmp"),
+	}
+}
 
 func getDataDir() string {
+	// Check if we're in a Docker container (common location)
 	if _, err := os.Stat("/data"); err == nil {
 		return "/data"
 	}
-	// Local: make it absolute relative to working directory
+
+	// Local development: relative to working directory
 	wd, err := os.Getwd()
 	if err != nil {
-		panic(err)
+		panic("Failed to get working directory: " + err.Error())
 	}
+
 	return filepath.Join(wd, "data")
 }
 
 func EnsureDirectories() error {
 	dirs := []string{
-		DataDir,
-		fmt.Sprintf("%s/sourceVideo", DataDir),
-		fmt.Sprintf("%s/video", DataDir),
-		fmt.Sprintf("%s/logs", DataDir),
-		fmt.Sprintf("%s/tmp", DataDir),
+		AppPaths.Data,
+		AppPaths.SourceVideo,
+		AppPaths.Video,
+		AppPaths.Logs,
+		AppPaths.Tmp,
 	}
 
 	for _, dir := range dirs {
--- internal/rest/rest.go
+++ internal/rest/rest.go
@@ -65,9 +65,8 @@ func (rest *Rest) GetVideoInfo(w http.ResponseWriter, r *http.Request) {
 
 func (rest *Rest) Transcode(w http.ResponseWriter, r *http.Request) {
 	params := r.PathValue("params")
-	// TODO put all paths in config
-	inputPath := filepath.Join(config.DataDir, "sourceVideo", "bunny.mp4")
-	outputPath := config.VideoDir
+	inputPath := filepath.Join(config.AppPaths.SourceVideo, "bunny.mp4")
+	outputPath := config.AppPaths.Video
 	resultCh, errCh := rest.videoService.Transcode(r.Context(), params, inputPath, outputPath)
 
 	select {
