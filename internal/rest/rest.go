93183c08040270f75b69c44f8bf59ac51ab4adaf
Guntis <guntis.smaukstelis@gmail.com>
2025-12-08 23:11:39 +0200

    Small cleanup

--- internal/rest/rest.go
+++ internal/rest/rest.go
@@ -36,28 +36,6 @@ func (rest *Rest) ServeStaticFiles(w http.ResponseWriter, r *http.Request) {
 	fs.ServeHTTP(w, r)
 }
 
-func (rest *Rest) ServeVideo(w http.ResponseWriter, r *http.Request) {
-	params := r.PathValue("params")
-
-	w.Header().Set("Accept-Ranges", "bytes")
-
-	resultCh, errCh := rest.videoService.GetOrGenerate(r.Context(), params)
-
-	select {
-	case videoPath := <-resultCh:
-		ext := strings.TrimPrefix(filepath.Ext(videoPath), ".")
-		w.Header().Set("Content-Type", "video/"+ext)
-
-		http.ServeFile(w, r, videoPath)
-
-	case err := <-errCh:
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-
-	case <-r.Context().Done():
-		http.Error(w, "request cancelled", http.StatusRequestTimeout)
-	}
-}
-
 func (rest *Rest) GetVideoInfo(w http.ResponseWriter, r *http.Request) {
 	name := r.PathValue("name")
 	info, err := rest.videoService.GetInfo(name)
@@ -85,6 +63,28 @@ func (rest *Rest) Transcode(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(map[string]string{"output": result})
 	case err := <-errCh:
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	case <-r.Context().Done():
+		http.Error(w, "request cancelled", http.StatusRequestTimeout)
+	}
+}
+
+func (rest *Rest) ServeVideo(w http.ResponseWriter, r *http.Request) {
+	params := r.PathValue("params")
+
+	w.Header().Set("Accept-Ranges", "bytes")
+
+	resultCh, errCh := rest.videoService.GetOrGenerate(r.Context(), params)
+
+	select {
+	case videoPath := <-resultCh:
+		ext := strings.TrimPrefix(filepath.Ext(videoPath), ".")
+		w.Header().Set("Content-Type", "video/"+ext)
+
+		http.ServeFile(w, r, videoPath)
+
+	case err := <-errCh:
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
 	case <-r.Context().Done():
 		http.Error(w, "request cancelled", http.StatusRequestTimeout)
 	}
