c138dcad281b34c904c34e5fc3e7926d46ce4c09
Guntis Smaukstelis <gsmaukstelis@evolution.com>
2025-12-11 16:33:18 +0200

    Remove lorem.video from referrer in stats aggregator

--- cmd/stats/main.go
+++ cmd/stats/main.go
@@ -19,6 +19,7 @@ func main() {
 	var (
 		excludeStatic  = flag.Bool("exclude-static", true, "Exclude /web/... paths")
 		excludePartial = flag.Bool("exclude-partial", true, "Exclude partial content (206 responses)")
+		excludeReferer = flag.String("exclude-referer", "lorem.video", "Exclude referrers containing this domain (empty to include all)")
 		minDate        = flag.String("min-date", defaultMinDate, "Minimum date YYYY-MM-DD (default: 7 days ago)")
 		maxDate        = flag.String("max-date", "", "Maximum date YYYY-MM-DD (empty for all)")
 		topN           = flag.Int("top", 20, "Number of top results to show")
@@ -29,6 +30,7 @@ func main() {
 	analyzerConfig := stats.AnalyzerConfig{
 		ExcludeStaticPaths: *excludeStatic,
 		ExcludePartial:     *excludePartial,
+		ExcludeReferer:     *excludeReferer,
 		MinDate:            *minDate,
 		MaxDate:            *maxDate,
 	}
