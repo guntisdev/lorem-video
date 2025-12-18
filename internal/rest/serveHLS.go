4556c3fa0eee286ab8f5a7dad3c698da69c4e9c0
Guntis <guntis.smaukstelis@gmail.com>
2025-12-18 21:04:54 +0200

    Dynamic hls media playlist and chunk selection

--- internal/rest/serveHLS.go
+++ internal/rest/serveHLS.go
@@ -1,11 +1,14 @@
 package rest
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 
 	"lorem.video/internal/config"
 )
@@ -23,54 +26,116 @@ func (r *Rest) ServeHLS(w http.ResponseWriter, req *http.Request) {
 		path = config.HLSMasterPlaylist
 	}
 
-	videoDir := filepath.Join(config.AppPaths.Stream, videoName)
-	if _, err := os.Stat(videoDir); os.IsNotExist(err) {
+	videoNameDir := filepath.Join(config.AppPaths.Stream, videoName)
+	if _, err := os.Stat(videoNameDir); os.IsNotExist(err) {
 		http.Error(w, "Video not found", http.StatusNotFound)
 		return
 	}
 
-	// TODO generate dynimcally based on current time and chunk count
-	if strings.HasSuffix(path, "/media.m3u8") {
-		playlist := `#EXTM3U
-#EXT-X-VERSION:7
-#EXT-X-TARGETDURATION:8
-#EXT-X-MEDIA-SEQUENCE:1
-#EXT-X-MAP:URI="init.mp4"
-#EXTINF:8.356546,
-chunk_001.mp4
-#EXTINF:4.579387,
-chunk_002.mp4
-#EXTINF:5.080780,
-chunk_003.mp4
-#EXTINF:8.356546,
-chunk_004.mp4
-#EXTINF:1.738162,
-chunk_005.mp4
-#EXT-X-ENDLIST
-`
+	fullPath := filepath.Join(videoNameDir, path)
+
+	// log.Printf(fullPath)
+
+	// /bunny/playlist.m3u8
+	if path == config.HLSMasterPlaylist {
 		w.Header().Set("Content-Type", "application/vnd.apple.mpegurl")
-		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Cache-Control", "public, max-age=1")
+		http.ServeFile(w, req, fullPath)
+		return
+	}
+
+	// /bunny/720p/media.m3u8
+	if strings.HasSuffix(path, "/"+config.HLSMediaPlaylist) {
+		resolutionKey := strings.TrimSuffix(path, "/"+config.HLSMediaPlaylist)
+		resolutionDir := filepath.Join(videoNameDir, resolutionKey)
+		if stat, err := os.Stat(resolutionDir); err != nil || !stat.IsDir() {
+			http.Error(w, "Resolution not found", http.StatusNotFound)
+			return
+		}
+
+		playlist := generateMediaPlaylist()
+
+		w.Header().Set("Content-Type", "application/vnd.apple.mpegurl")
+		w.Header().Set("Cache-Control", "public, max-age=1")
 		w.Write([]byte(playlist))
 		return
 	}
 
-	// Serve file from video directory
-	fullPath := filepath.Join(videoDir, path)
-
-	// Security: ensure we're still within video directory
-	if !strings.HasPrefix(filepath.Clean(fullPath), videoDir) {
-		http.Error(w, "Invalid path", http.StatusBadRequest)
+	// /bunny/720p/init.mp4
+	if strings.HasSuffix(path, "/"+config.HLSInit) {
+		w.Header().Set("Content-Type", "video/mp4")
+		// w.Header().Set("Cache-Control", "public, max-age=31536000")
+		w.Header().Set("Cache-Control", "public, max-age=1")
+		http.ServeFile(w, req, fullPath)
 		return
 	}
 
-	// Set content type
-	contentType := "video/mp4"
-	if strings.HasSuffix(path, ".m3u8") {
-		contentType = "application/vnd.apple.mpegurl"
+	filename := filepath.Base(fullPath)
+	// /bunny/720p/media.1679654321.mp4
+	if strings.HasPrefix(filename, "media.") && strings.HasSuffix(filename, ".mp4") {
+		resolutionKey := strings.TrimSuffix(path, "/"+filename)
+		resolutionDir := filepath.Join(videoNameDir, resolutionKey)
+		if stat, err := os.Stat(resolutionDir); err != nil || !stat.IsDir() {
+			http.Error(w, "Resolution not found", http.StatusNotFound)
+			return
+		}
+
+		// TODO maybe better pattern match from config.HLSChunkFormat
+		chunkPattern := filepath.Join(resolutionDir, "chunk_*.mp4")
+		matches, err := filepath.Glob(chunkPattern)
+		if err != nil {
+			http.Error(w, "Error reading chunks", http.StatusInternalServerError)
+			return
+		}
+
+		// IMPORTANT: exclude last segment as it may not be full second and wouldn't loop infinitely
+		chunkCount := len(matches) - 1
+
+		if chunkCount < 1 {
+			http.Error(w, "No chunks found", http.StatusNotFound)
+			return
+		}
+
+		hlsSeqStr := strings.TrimSuffix(strings.TrimPrefix(filename, "media."), ".mp4")
+		hlsSeq, err := strconv.ParseInt(hlsSeqStr, 10, 64)
+		if err != nil {
+			http.Error(w, "Invalid sequence number", http.StatusNotFound)
+			return
+		}
+
+		chunkId := int(hlsSeq % int64(chunkCount))
+		chunkName := fmt.Sprintf("chunk_%03d.mp4", chunkId)
+		log.Printf("%s", chunkName)
+		chunkFile := filepath.Join(filepath.Dir(fullPath), chunkName)
+
+		w.Header().Set("Content-Type", "video/mp4")
+		// w.Header().Set("Cache-Control", "public, max-age=31536000")
+		w.Header().Set("Cache-Control", "public, max-age=1")
+		http.ServeFile(w, req, chunkFile)
+		return
 	}
 
-	w.Header().Set("Content-Type", contentType)
-	w.Header().Set("Cache-Control", "public, max-age=31536000")
+	http.Error(w, "No hls found", http.StatusNotFound)
+}
 
-	http.ServeFile(w, req, fullPath)
+func generateMediaPlaylist() string {
+	const segmentsToServe = 5
+
+	now := time.Now().Unix()
+
+	var chunks strings.Builder
+	chunks.WriteString("#EXTM3U\n")
+	chunks.WriteString("#EXT-X-VERSION:7\n")
+	chunks.WriteString("#EXT-X-TARGETDURATION:1\n")
+	chunks.WriteString(fmt.Sprintf("#EXT-X-MEDIA-SEQUENCE:%d\n", now))
+	chunks.WriteString("#EXT-X-MAP:URI=\"init.mp4\"\n")
+
+	for i := 0; i < segmentsToServe; i++ {
+		chunks.WriteString("#EXTINF:1.000000,\n")
+		chunks.WriteString(fmt.Sprintf("media.%d.mp4\n", now+int64(i)))
+	}
+
+	// log.Printf(chunks.String())
+
+	return chunks.String()
 }
